bf1718bc0f1a92f92e878bdd6e791a1506eb6dcd
Anderson S. Vieira <svieira.anderson@gmail.com>
2022-05-11 11:13:48 -0300

    declara atributos e métodos

--- ListaAquecimento/src/Canal.java
+++ ListaAquecimento/src/Canal.java
@@ -1,4 +1,33 @@
 
 public class Canal {
 
+	private String nomeCanal;
+	private String tipoCanal;
+	
+	
+	public Canal(String n, String t) {
+		nomeCanal = n;
+		tipoCanal = t;
+	}
+	
+	public Canal() {
+	}
+	
+	public String toString() {
+		return nomeCanal;
+	}
+	
+	public boolean equals(Canal c) {
+		return nomeCanal.equals(c.getNomeCanal());
+	}
+
+	public String getNomeCanal() {
+		return nomeCanal;
+	}
+
+	public String getTipoCanal() {
+		return tipoCanal;
+	}
+	
+	
 }
