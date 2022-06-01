77c1fe5283c9a0f7cc7967ce46fb803fb03f63a9
Anderson S. Vieira <svieira.anderson@gmail.com>
2022-06-01 10:37:52 -0300

    cria classe Mensageiro (sem senha)

--- ListaAquecimento/src/metodos/Mensageiro.java
+++ ListaAquecimento/src/metodos/Mensageiro.java
@@ -1,5 +1,51 @@
 package metodos;
 
+import java.util.Properties;
+
+import jakarta.mail.Address;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+
 public class Mensageiro {
 
+	public static void enviarProgramacaoDeHoje(String email) {
+
+		Properties props = new Properties();
+
+		props.put("mail.smtp.user", "projetopoo2022.1@gmail.com"); 
+		props.put("mail.smtp.host", "smtp.gmail.com"); 
+		props.put("mail.smtp.port", "25");
+		props.put("mail.debug", "true"); 
+		props.put("mail.smtp.auth", "true"); 
+		props.put("mail.smtp.starttls.enable", "true"); 
+		props.put("mail.smtp.EnableSSL.enable", "true");
+		props.setProperty("mail.smtp.socketFactory.class", "jakarta.net.ssl.SSLSocketFactory");
+		props.setProperty("mail.smtp.socketFactory.fallback", "false");
+		props.setProperty("mail.smtp.port", "465");
+		props.setProperty("mail.smtp.socketFactory.port", "465");
+
+		Session session = Session.getDefaultInstance(props, new jakarta.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+			}
+		});
+
+		session.setDebug(true); 
+
+		try {
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("projetopoo2022.1@gmail.com"));
+			Address[] toUser = InternetAddress.parse("mariabitargino123@gmail.com, svieira.anderson@gmail.com");
+			message.setRecipients(Message.RecipientType.TO, toUser); 
+			message.setSubject("Testando JavaMail"); 
+			message.setText("enviando email");
+			Transport.send(message);
+		} catch (MessagingException e) { 
+			e.printStackTrace();
+		}
+	}
 }
