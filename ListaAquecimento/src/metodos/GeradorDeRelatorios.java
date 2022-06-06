c725519d40ca902caeb229ca2dbb1d3f2970af30
Maria Beatriz <88341459+BeatrizTargino@users.noreply.github.com>
2022-06-06 16:43:17 -0300

    Add files via upload
    classe Gerador de relatorios corrigida

--- ListaAquecimento/src/metodos/GeradorDeRelatorios.java
+++ ListaAquecimento/src/metodos/GeradorDeRelatorios.java
@@ -1,7 +1,7 @@
-package metodos;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
@@ -9,29 +9,35 @@ import com.itextpdf.text.PageSize;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
 
-import midia.Canal;
-
 public class GeradorDeRelatorios {
 
 	public static void obterProgramacaoDeUmCanal(Canal canal) {
 
-		Document doc = new Document(PageSize.A4);
-
+		Persistencia persistencia = new Persistencia();
+		CentralDeInformacoes central = persistencia.recuperarCentral("central");
+		Document documento = new Document(PageSize.A4, 72, 72, 72, 72);
 		try {
-			OutputStream os = new FileOutputStream("relatorio.pdf");
-			PdfWriter.getInstance(doc, os);
-
-			doc.open();
-
-			Paragraph pg = new Paragraph();
-			doc.add(pg);
-
-			doc.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (DocumentException e) {
+			PdfWriter.getInstance(documento, new FileOutputStream("relatorio.pdf"));
+			documento.open();
+			Paragraph p1 = new Paragraph("### Programas criados do canal " + canal + "###\\n");
+					documento.add(p1);
+			ArrayList<ProgramaDeTv> programas =
+					central.buscarProgramasPorCanal(canal.getNome());
+			if (programas.isEmpty()) {
+				Paragraph p = new Paragraph("O canal " + canal.getNome() + "não possui programas");
+						documento.add(p);
+			} else {
+				for (ProgramaDeTv programa : programas) {
+					Paragraph p = new Paragraph(programa.toString());
+					documento.add(p);
+				}
+			}
+			documento.close();
+		} catch (FileNotFoundException | DocumentException e) {
 			e.printStackTrace();
 		}
 	}
-
 }
+
+
+
