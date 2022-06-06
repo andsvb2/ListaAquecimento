11d16ded7a4b4dd077a5fb23b629d067a3fa5c73
Maria Beatriz <88341459+BeatrizTargino@users.noreply.github.com>
2022-06-06 16:45:03 -0300

    Add files via upload
    Classe Persistencia corrigida

--- ListaAquecimento/src/metodos/Persistencia.java
+++ ListaAquecimento/src/metodos/Persistencia.java
@@ -1,35 +1,54 @@
-package metodos;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
 
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
+import com.thoughtworks.xstream.security.NoTypePermission;
+import com.thoughtworks.xstream.security.NullPermission;
+import com.thoughtworks.xstream.security.PrimitiveTypePermission;
 
 public class Persistencia {
 
 	private XStream xstream = new XStream(new DomDriver("UTF-8"));
-
-	public void salvarCentral(CentralDeInformacoes central) throws Exception {
-		File arquivo = new File("dados.xml");
-		arquivo.createNewFile();
-
-		PrintWriter pw = new PrintWriter(arquivo);
-		String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n";
-		xml = xstream.toXML(central);
-		pw.print(xml);
-		pw.close();
+	
+	public Persistencia() {
+		xstream.addPermission(NoTypePermission.NONE); //forbid everything
+		xstream.addPermission(NullPermission.NULL); // allow "null"
+		xstream.addPermission(PrimitiveTypePermission.PRIMITIVES); // allow primitive types
+		xstream.allowTypes( new Class[] {CentralDeInformacoes.class, Persistencia.class,
+				GeradorDeRelatorios.class,
+				Canal.class, ProgramaDeTv.class});
 	}
-
-	public CentralDeInformacoes recuperarCentral() throws Exception {
-
-		File arquivo = new File("dados.xml");
-
-		if (arquivo.exists()) {
-			FileInputStream f = new FileInputStream(arquivo);
-			return (CentralDeInformacoes) xstream.fromXML(f);
-		} else {
-			return new CentralDeInformacoes();
+	
+	public void salvarCentral(CentralDeInformacoes central, String nome) {
+		File arquivoPadrao = new File(nome + ".xml");
+		String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n";
+		xml += xstream.toXML(central);
+		try {
+			PrintWriter gravar = new PrintWriter(arquivoPadrao);
+			arquivoPadrao.createNewFile();
+			gravar.print(xml);
+			gravar.close();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
+	
+	public CentralDeInformacoes recuperarCentral(String nome) {
+		File arquivoPadrao = new File(nome + ".xml");
+		try {
+			if (arquivoPadrao.exists()) {
+				FileInputStream fis = new FileInputStream(arquivoPadrao);
+				return (CentralDeInformacoes) xstream.fromXML(fis);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return new CentralDeInformacoes();
+	}
 }
+
+
