2132e5cbf6e7ae076ab7d58ccf1718f31661cf97
Maria Beatriz <88341459+BeatrizTargino@users.noreply.github.com>
2022-06-06 17:33:37 -0300

    Add files via upload
    Classe main corrigida

--- ListaAquecimento/src/metodos/MainAgenda.java
+++ ListaAquecimento/src/metodos/MainAgenda.java
@@ -1,88 +1,156 @@
-package metodos;
-
-import java.time.DayOfWeek;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
-import programas.TipoDePrograma;
-
 public class MainAgenda {
-	
-	private static void exibirMenu() {
-		System.out.println(
-				"1 - cadastrar novo programa;"
-				+ "\n2 - listar todos os programas;"
-				+ "\n3 - listar os programas de um tipo;"
-				+ "\n4 - cadastrar novo canal de televis√£o;"
-				+ "\n5 - listar todos os canais de televis√£o;"
-				+ "\nS - sair");	
-	}
-	
-	private static void cadastrarPrograma(Scanner input) {
-		
-		System.out.print("Nome do programa: ");
-		String nome = input.nextLine();
-		
-		System.out.print("Tipo do programa:"
-				+ "\n1 - S√©rie Regular;"
-				+ "\n2 - Reality show;"
-				+ "\n3 - Programa cont√≠nuo.");
-		TipoDePrograma tipoPrograma = TipoDePrograma.of(Integer.parseInt(input.nextLine()));
-		
-		System.out.print("Quantos dias de exibi√ß√£o por semana?");
-		int qtdDias = Integer.parseInt(input.nextLine());
-		
-		ArrayList<DayOfWeek> diasExibicao = new ArrayList<DayOfWeek>();
-		
-		System.out.print("Quais os dias de exibi√ß√£o na semana?"
-				+ "\n1 - segunda;"
-				+ "\n2 - ter√ßa;"
-				+ "\n3 - quarta;"
-				+ "\n4 - quinta;"
-				+ "\n5 - sexta;"
-				+ "\n6 - s√°bado;"
-				+ "\n7 - domingo.");
-		
-		for(int i = 0; 0 < qtdDias;) {
-			DayOfWeek dia = DayOfWeek.of(Integer.parseInt(input.nextLine()));
-			
-			if (diasExibicao.contains(dia) == false) {
-				diasExibicao.add(dia);
-				i++;
-			}
-		}
-	}
 
 	public static void main(String[] args) {
+		Scanner leia = new Scanner(System.in);
+
+		Persistencia persistencia = new Persistencia();
+		CentralDeInformacoes central = persistencia.recuperarCentral("central");
+		Canal canal = new Canal();
 		
-		Scanner input = new Scanner(System.in);
-		
-		String opcao = null;
-		
-		while (!opcao.equals("S")) {
+		boolean sair = false;
+		do {
+			System.out.println("[1] - Novo Programa");
+			System.out.println("[2] - Listar Programas");
+			System.out.println("[3] - Listar programas de um mesmo tipo");
+			System.out.println("[4] - Novo canal");
+			System.out.println("[5] - Listar canais");
+			System.out.println("[6] - Gerar relaÁ„o dos programas de um mesmo canal");
+			System.out.println("[7] - Enviar programaÁ„o de hoje");
+			System.out.println("[S] - Sair");
+			System.out.print("\nOpÁ„o: ");
 			
-			exibirMenu();
-			
-			opcao = input.nextLine();
-			
+			String opcao = leia.nextLine();
 			switch (opcao) {
-			
 			case "1":
-				cadastrarPrograma(input);
+				if (central.getListaCanais().isEmpty()) {
+					System.out.println("Para cadastrar um programa È necess·rio cadastrar um Canal(s) previamente.\n");
+				} else {
+					System.out.println("\n-- dados do Programa -- ");
+
+					System.out.print("Nome do Programa: ");
+					String nome = leia.nextLine();
+
+					System.out.print("Tipo do Programa: ");
+					central.getTodosOsProgramas();
+
+					System.out.print("\nOpÁ„o: ");
+					String tipoString = leia.nextLine().toUpperCase();
+
+					System.out.print("Canais: ");
+					central.getListaCanais();
+
+					System.out.print("\nCanal: ");
+					String nomeCanal = leia.nextLine();
+
+					canal = central.recuperarCanalPeloNome(nomeCanal);
+					
+					if(canal == null) {
+						System.out.println("n„o existe esse canal: " +
+								nomeCanal);
+						break;
+					}
+
+					System.out.println("Dias da Semana: ");
+					System.out.print("Dias escolhidos: ");
+					String[] dias = leia.nextLine().split(",");
+
+					ArrayList<String> diasDaSemana = new
+							ArrayList<>(Arrays.asList(dias));
+
+					ProgramaDeTv programa = new ProgramaDeTv(nomeCanal, null, null, canal);
+					if (central.adicionarProgramaDeTv(programa)) {
+						System.out.println("Programa cadastrado com sucesso!");
+						persistencia.salvarCentral(central, "central");
+					} else {
+						System.out.println("n„o foi possÌvel cadastrar o programa!");
+						System.out.println("O programa j· existe ou n„o possui um canal.");
+					}
+				}
 				break;
 			case "2":
+				if (central.getTodosOsProgramas().isEmpty()) {
+					System.out.println("N„o h· nenhum programa cadastrado.");
+				} else {
+					central.getTodosOsProgramas();
+				}
 				break;
 			case "3":
+				System.out.print("Tipo do Programa: ");
+				canal.getTipoCanal();
+				
+				System.out.print("\nOpÁ„o: ");
+				String tipoString = leia.nextLine().toUpperCase();
+				
+				if(canal.getTipoCanal().contains(tipoString)) {
+					canal.getTipoCanal();
+				} else {
+					System.out.println("n„o existe esse tipo de programa : " +
+							tipoString);
+					break;
+				}
 				break;
 			case "4":
+				System.out.println("-- dados do canal --");
+				
+				System.out.print("nome: ");
+				String nomeCanal = leia.nextLine();
+				
+				System.out.print("Tipo do canal: ");
+				String tipoCanal = leia.nextLine();
+				
+				if (central.adicionarCanal(canal)) {
+					System.out.println("Canal cadastrado com sucesso!\n");
+					persistencia.salvarCentral(central, "central");
+				} else {
+					System.out.println("n„o foi possÌvel cadastrar o canal!");
+					System.out.println("O canal j· existe.");
+				}
 				break;
 			case "5":
+				if (central.getListaCanais().isEmpty()) {
+					System.out.println("nenhum canal cadastrado.\n");
+				} else {
+					central.getListaCanais();
+				}
+				break;
+			case "6":
+				System.out.print("Canais: ");
+				central.getListaCanais();
+				
+				System.out.print("\nOpÁ„o: ");
+				String nomeDoCanal = leia.nextLine();
+				
+				canal = central.recuperarCanalPeloNome(nomeDoCanal);
+				if( canal != null) {
+					GeradorDeRelatorios.obterProgramacaoDeUmCanal(canal);
+				} else {
+					System.out.println("N„o foi encontrado um canal com esse nome");
+				}
+				break;
+			case "7":
+				System.out.print("informe seu e-mail: ");
+				String destinatario = leia.nextLine();
+				
+				System.out.println("enviando e-mail...");
+				Mensageiro.enviarProgramacaoDeHoje("ProgramaÁ„o do dia");
+				System.out.println("e-mail enviado. cheque sua caixa de entrada ou spam");
+				break;
+			case "s":
+			case "S":
+				System.out.println("Fim de programa");
+				sair = true;
 				break;
 			default:
+				System.out.println("OpÁ„o inv·lida.");
 				break;
 			}
-			
-		}
-		
+		} while (!sair);
 	}
+
 }
+
+
