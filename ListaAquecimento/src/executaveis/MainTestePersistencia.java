2e6156b6bfd5968720badf1753dfc3b21810c16f
Anderson S. Vieira <svieira.anderson@gmail.com>
2022-07-01 17:13:24 -0300

    testa a persistência

--- ListaAquecimento/src/executaveis/MainTestePersistencia.java
+++ ListaAquecimento/src/executaveis/MainTestePersistencia.java
@@ -1,18 +1,40 @@
 package executaveis;
 
+import java.time.DayOfWeek;
+import java.util.ArrayList;
+
 import metodos.CentralDeInformacoes;
 import metodos.Persistencia;
+import midia.Canal;
+import programas.ProgramaDeTv;
+import programas.TipoDePrograma;
 
 public class MainTestePersistencia {
 
 	public static void main(String[] args) {
 		
 		Persistencia pe = new Persistencia();
-		CentralDeInformacoes central = pe.recuperarCentral("central");
+		CentralDeInformacoes central = new CentralDeInformacoes();
 		
+		Canal c = new Canal("SBT", "Canal aberto");
 		
+		ArrayList<DayOfWeek> diasExibicao = new ArrayList<>();
 		
+		diasExibicao.add(DayOfWeek.MONDAY);
+		diasExibicao.add(DayOfWeek.TUESDAY);
+		diasExibicao.add(DayOfWeek.WEDNESDAY);
+		diasExibicao.add(DayOfWeek.THURSDAY);
+		diasExibicao.add(DayOfWeek.FRIDAY);
+		diasExibicao.add(DayOfWeek.SATURDAY);
 
+		ProgramaDeTv pdtv = new ProgramaDeTv("Jornal da Noite", TipoDePrograma.PC, diasExibicao, c);
+		
+		
+		central.adicionarCanal(c);
+		central.adicionarProgramaDeTv(pdtv);
+		
+		pe.salvarCentral(central);
+		
 	}
 
 }
