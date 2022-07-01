fe5641561e4ae4022fae1250e1e1235d8f4193bf
Anderson S. Vieira <svieira.anderson@gmail.com>
2022-07-01 17:47:14 -0300

    cria um validador de email

---
+++ ListaAquecimento/src/metodos/ValidadorEmail.java
@@ -0,0 +1,24 @@
+/**
+ * @author https://receitasdecodigo.com.br/java/validar-email-em-java
+ *
+ */
+
+package metodos;
+
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
+public abstract class ValidadorEmail {
+	
+	public static boolean isValidEmailAddress(String email) {
+		boolean result = true;
+		try {
+			InternetAddress emailAddr = new InternetAddress(email);
+			emailAddr.validate();
+		} catch (AddressException ex) {
+			result = false;
+		}
+		return result;
+	}
+
+}
