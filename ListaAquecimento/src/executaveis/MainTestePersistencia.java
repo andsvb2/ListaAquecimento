b0ddf4d772c707dba7825d42c8b78b32ae55e3b9
Anderson S. Vieira <svieira.anderson@gmail.com>
2022-07-29 15:28:14 -0300

    ajusta o teste da persistência

--- ListaAquecimento/src/executaveis/MainTestePersistencia.java
+++ ListaAquecimento/src/executaveis/MainTestePersistencia.java
@@ -1,9 +1,12 @@
 package executaveis;
 
 import java.time.DayOfWeek;
+import java.time.LocalTime;
 import java.util.ArrayList;
 
-import enums.TipoDePrograma;
+import enums.EstiloSerieRegular;
+import enums.StatusPrograma;
+import enums.TipoCanal;
 import metodos.CentralDeInformacoes;
 import metodos.Persistencia;
 import midia.Canal;
@@ -16,7 +19,7 @@ public class MainTestePersistencia {
 		Persistencia pe = Persistencia.getInstancia();
 		CentralDeInformacoes central = pe.recuperarCentral();
 		
-		Canal c = new Canal("SBT", "Canal aberto");
+		Canal c = new Canal("SBT", TipoCanal.ABERTO);
 		
 		ArrayList<DayOfWeek> diasExibicao = new ArrayList<>();
 		
@@ -26,12 +29,19 @@ public class MainTestePersistencia {
 		diasExibicao.add(DayOfWeek.THURSDAY);
 		diasExibicao.add(DayOfWeek.FRIDAY);
 		diasExibicao.add(DayOfWeek.SATURDAY);
+		
+		LocalTime hora = LocalTime.of(21, 15);
+		StatusPrograma status = StatusPrograma.EXIBINDO;
+		int temporadas = 5;
+		EstiloSerieRegular estilo = EstiloSerieRegular.LA;
+		String genero = "Jornal";
 
-		SerieRegular pdtv = new SerieRegular("Jornal da Noite", TipoDePrograma.PC, diasExibicao, c);
+		SerieRegular jornal = new SerieRegular("Jornal da Noite", c, hora, 
+				status, diasExibicao, temporadas, estilo, genero);
 		
 		
 		central.adicionarCanal(c);
-		central.adicionarProgramaDeTv(pdtv);
+		central.adicionarPrograma(jornal);
 		
 		pe.salvarCentral(central);
 		
