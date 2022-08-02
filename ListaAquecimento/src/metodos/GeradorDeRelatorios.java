d9dc63a7ca8d47050e306e42b56d45068c5f5d20
Maria Beatriz <88341459+BeatrizTargino@users.noreply.github.com>
2022-08-02 15:35:34 -0300

    Update GeradorDeRelatorios.java

--- ListaAquecimento/src/metodos/GeradorDeRelatorios.java
+++ ListaAquecimento/src/metodos/GeradorDeRelatorios.java
@@ -3,15 +3,13 @@ package metodos;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
-import java.util.ArrayList;
 
-import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.PageSize;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
 
-import midia.Canal;
+import com.itextpdf.text.Document;
 
 public class GeradorDeRelatorios {
 
@@ -40,7 +38,4 @@ public class GeradorDeRelatorios {
 	}
 
 }
-
-
-
 
